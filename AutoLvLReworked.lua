ac43a185b6602db442a786a0007b930e0d8dcb4b
CooLowbro <pulquerio@me.com>
2016-01-06 22:14:14 -0200

    Update AutoLvLReworked.lua

--- AutoLvLReworked.lua
+++ AutoLvLReworked.lua
@@ -4,7 +4,7 @@ local _autoLevel = { spellsSlots = { SPELL_1, SPELL_2, SPELL_3, SPELL_4 }, level
 local __autoLevel__OnTick
 local rOFF=0
 --update func--
-local version = "1.03"
+local version = "1.04"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/CooLowbro/BoL/master/AutoLvLReworked.lua".."?rand="..math.random(1,10000)
@@ -212,19 +212,19 @@ end end
 
 _G.LevelSpell = function(id)
   local offsets = { 
-    [_Q] = 0x61,
-    [_W] = 0x81,
-    [_E] = 0xA1,
-    [_R] = 0xC1,
+    [_Q] = 0x1E,
+    [_W] = 0xD3,
+    [_E] = 0x3A,
+    [_R] = 0xA8,
   }
-  local p = CLoLPacket(0x0033)
-  p.vTable = 0xE556E8
+  local p = CLoLPacket(0x00B6)
+  p.vTable = 0xFE3124
   p:EncodeF(myHero.networkID)
-  p:Encode1(0x73)
-  for i = 1, 4 do p:Encode1(0xF9) end
-  for i = 1, 4 do p:Encode1(0x1E) end
+  p:Encode1(0xC1)
   p:Encode1(offsets[id])
-  for i = 1, 4 do p:Encode1(0x99) end
+  for i = 1, 4 do p:Encode1(0x63) end
+  for i = 1, 4 do p:Encode1(0xC5) end
+  for i = 1, 4 do p:Encode1(0x6A) end
   for i = 1, 4 do p:Encode1(0x00) end
   SendPacket(p)
 end
